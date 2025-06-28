b199782566d791bfd18d0098e5514f8384c2ef8c
mohan-bee <mohn08052006@gmail.com>
2025-06-28 15:11:44 +0530

    deployment

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import introMp4 from '/introducing.mp4'; // Ensure correct relative path
+
 import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
@@ -12,7 +12,7 @@ const Landing = () => {
     <div className="w-full h-screen overflow-hidden">
         {showBtn && <button onClick={() => navigate('/login')} className='z-10 absolute transition-all bg-amber-100 p-3 rounded-md shadow-2xs bottom-20 right-1/4 border text-xl sm:text-xl font-bold text-zinc-800'>Get Started</button>}
       <video
-        src={introMp4}
+        src="/Introducing.mp4"
         autoPlay
         muted
         loop
